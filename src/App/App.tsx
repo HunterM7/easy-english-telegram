a082a2b642499e794a105566fadf2ddcbe742a44
Anton Meshchaninov <tonymsnv@gmail.com>
2025-08-17 15:34:33 +0300

    Test.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,77 +1,76 @@
 import { useEffect, useState } from 'react'
 import { init, initDataUser } from '@telegram-apps/sdk'
-import { isTMA, mockTelegramEnv } from '@telegram-apps/bridge';
-import './App.css'
+import { isTMA, mockTelegramEnv, emitEvent } from '@telegram-apps/bridge';
 
 export function App() {
-  const [initData, setInitData] = useState({})
+  const [initData, setInitData] = useState<object | undefined>(undefined)
+
+  const noInsets = {
+    left: 0,
+    top: 0,
+    bottom: 0,
+    right: 0,
+  } as const;
+
+  const themeParams = {
+    accent_text_color: '#6ab2f2',
+    bg_color: '#17212b',
+    button_color: '#5288c1',
+    button_text_color: '#ffffff',
+    destructive_text_color: '#ec3942',
+    header_bg_color: '#17212b',
+    hint_color: '#708499',
+    link_color: '#6ab3f3',
+    secondary_bg_color: '#232e3c',
+    section_bg_color: '#17212b',
+    section_header_text_color: '#6ab3f3',
+    subtitle_text_color: '#708499',
+    text_color: '#f5f5f5',
+  } as const;
 
   useEffect(() => {
     if (isTMA()) {
       // Telegram Mini Apps environment
-      init()
-    } else {
-      // Debug environment
+      console.log('Telegram Mini Apps environment')
       mockTelegramEnv({
         launchParams: {
-          tgWebAppPlatform: 'web',
-          tgWebAppVersion: '59',
-          tgWebAppThemeParams: {
-            accent_text_color: '#6ab2f2',
-            bg_color: '#17212b',
-            button_color: '#5288c1',
-            button_text_color: '#ffffff',
-            destructive_text_color: '#ec3942',
-            // headerBgColor: '#17212b',
-            // hintColor: '#708499',
-            // linkColor: '#6ab3f3',
-            // secondaryBgColor: '#232e3c',
-            // sectionBgColor: '#17212b',
-            // sectionHeaderTextColor: '#6ab3f3',
-            // subtitleTextColor: '#708499',
-            // textColor: '#f5f5f5',
-          },
+          tgWebAppThemeParams: themeParams,
+          tgWebAppData: new URLSearchParams([
+            ['user', JSON.stringify({
+              id: 1,
+              first_name: 'Pavel',
+            })],
+            ['hash', ''],
+            ['signature', ''],
+            ['auth_date', Date.now().toString()],
+          ]),
+          tgWebAppStartParam: 'debug',
+          tgWebAppVersion: '8',
+          tgWebAppPlatform: 'tdesktop',
         },
-        
-        // initData: {
-        //   user: {
-        //     id: 99281932,
-        //     firstName: 'Andrew',
-        //     lastName: 'Rogue',
-        //     username: 'rogue',
-        //     languageCode: 'en',
-        //     isPremium: true,
-        //     allowsWriteToPm: true,
-        //   },
-        //   hash: '89d6079ad6762351f38c6dbbc41bb53048019256a9443988af7a48bcad16ba31',
-        //   authDate: new Date(1716922846000),
-        //   signature: 'abc',
-        //   startParam: 'debug',
-        //   chatType: 'sender',
-        //   chatInstance: '8428209589180549439',
-        // },
-        // initDataRaw: new URLSearchParams([
-        //   ['user', JSON.stringify({
-        //     id: 99281932,
-        //     first_name: 'Andrew',
-        //     last_name: 'Rogue',
-        //     username: 'rogue',
-        //     language_code: 'en',
-        //     is_premium: true,
-        //     allows_write_to_pm: true,
-        //   })],
-        //   ['hash', '89d6079ad6762351f38c6dbbc41bb53048019256a9443988af7a48bcad16ba31'],
-        //   ['auth_date', '1716922846'],
-        //   ['start_param', 'debug'],
-        //   ['signature', 'abc'],
-        //   ['chat_type', 'sender'],
-        //   ['chat_instance', '8428209589180549439'],
-        // ]).toString(),
-        // version: '7.2',
-        // platform: 'tdesktop',
+        onEvent(e) {
+          if (e[0] === 'web_app_request_theme') {
+            return emitEvent('theme_changed', { theme_params: themeParams });
+          }
+          if (e[0] === 'web_app_request_viewport') {
+            return emitEvent('viewport_changed', {
+              height: window.innerHeight,
+              width: window.innerWidth,
+              is_expanded: true,
+              is_state_stable: true,
+            });
+          }
+          if (e[0] === 'web_app_request_content_safe_area') {
+            return emitEvent('content_safe_area_changed', noInsets);
+          }
+          if (e[0] === 'web_app_request_safe_area') {
+            return emitEvent('safe_area_changed', noInsets);
+          }
+        },
       });
+      init()
     }
-    setInitData(initDataUser)
+    setInitData(initDataUser())
   }, [])
 
   return (
