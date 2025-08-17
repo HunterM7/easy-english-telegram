8007abcdff5d3026144ed1c97e82adb94469b02a
Anton Meshchaninov <tonymsnv@gmail.com>
2025-08-17 21:07:58 +0300

    Мелкие правки.

--- src/pages/lesson-page/lesson-page.tsx
+++ src/pages/lesson-page/lesson-page.tsx
@@ -1,10 +1,12 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './lesson-page.css';
 
 export function LessonPage() {
   const { id } = useParams();
   return (
     <div className='lesson-page'>
+      <Link to='/lessons'>Назад</Link>
+      <br />
       Страница урока №{id}
     </div>
   )
--- src/pages/lessons-page/lessons-page-card/lessons-page-card.tsx
+++ src/pages/lessons-page/lessons-page-card/lessons-page-card.tsx
@@ -12,7 +12,7 @@ interface Props {
 export function LessonsPageCard(props: Props) {
   const { id, name, description, className } = props;
   return (
-    <Link to={id.toString()} className={classNames('lessons-page-card', className)}>
+    <Link to={`/lessons/${id}`} className={classNames('lessons-page-card', className)}>
       <p className='lessons-page-card__name'>{name}</p>
       <p className='lessons-page-card__description'>{description}</p>
     </Link>
