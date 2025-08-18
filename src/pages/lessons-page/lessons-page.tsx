99ca5d652d471afba8255baa0b0a481ab2fbb12a
Anton Meshchaninov <tonymsnv@gmail.com>
2025-08-18 19:43:34 +0300

    Добавил кнопку возврата на страницу Уроков.

--- src/pages/lessons-page/lessons-page.tsx
+++ src/pages/lessons-page/lessons-page.tsx
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom';
 import { mockLessons } from './utils/mockLessons';
 import { LessonsPageCard } from './lessons-page-card/lessons-page-card';
 import './lessons-page.scss'
 
 export function LessonsPage() {
-
   return (
     <div className='lessons-page'>
-      <h1 className='lessons-page__title'>Уроки</h1>
+      <h1 className='lessons-page__title'>
+        <Link to='/' className='lessons-page__back-button'>Назад</Link>
+        Уроки
+      </h1>
       <div className='lessons-page__list'>
         {mockLessons.map(lesson => {
           const { id, name, description } = lesson;
