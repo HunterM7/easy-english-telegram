e7485c5b368c8b886291eb291e7c090496109084
Anton Meshchaninov <tonymsnv@gmail.com>
2025-08-20 20:27:22 +0300

    Добавил директорию requests.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: { '#src': '/src' },
+    alias: {
+      '#src': '/src',
+      '#requests': '/src/utils/requests',
+    },
   },
 })
