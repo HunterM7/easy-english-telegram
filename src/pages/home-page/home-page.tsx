9791aa3a8bdba5861c9dd2a3de369ad97596c4e9
Anton <tonym7@yandex.ru>
2026-02-08 17:47:59 +0300

    Данные от телеги. Временно.

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom';
 import './home-page.scss';
 
 export function HomePage() {
-  const { tgWebAppData } = useLaunchParams();
-  const firstName = tgWebAppData?.user?.first_name;
-  const photoUrl = tgWebAppData?.user?.photo_url;
+  const data = useLaunchParams();
+  const firstName = data.tgWebAppData?.user?.first_name;
+  const photoUrl = data.tgWebAppData?.user?.photo_url;
 
   return (
     <div className='home-page'>
       <span>Добро пожаловать{firstName ? `, ${firstName}` : ''}!</span>
       <img className='home-page__photo' src={photoUrl} alt={firstName} />
       <Link to='lessons'>Уроки</Link>
+      <div>{JSON.stringify(data)}</div>
     </div>
   )
 }
