a18b8c355c95d56b0d0cf210861342fe1aabc6b8
Anton Meshchaninov <tonymsnv@gmail.com>
2026-03-09 10:03:53 +0300

    Вместо localhost теперь слушаем текущий домен.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,9 +28,10 @@ async function bootstrap() {
   });
 
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  const host = process.env.HOST ?? '0.0.0.0';
+  await app.listen(port, host);
 
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on ${host}:${port}`);
 }
 
 void bootstrap();
